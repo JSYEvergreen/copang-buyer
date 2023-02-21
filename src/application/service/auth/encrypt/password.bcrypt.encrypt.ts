bf74c71acc3eb6fde85aae6abbdaa53f657aaecf
FonDitbul <niki7084@naver.com>
2023-02-22 00:44:42 +0900

    [COU-0] password encrypt 추가

---
+++ src/application/service/auth/encrypt/password.bcrypt.encrypt.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@nestjs/common';
+import { IPasswordEncryptor } from '../../../../domain/service/auth/encrypt/password.encrypt';
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class PasswordBcryptEncrypt implements IPasswordEncryptor {
+  async encrypt(rawPassword: string): Promise<string> {
+    const salt = await PasswordBcryptEncrypt.generateSalt();
+    return await bcrypt.hash(rawPassword, salt);
+  }
+
+  async compare(rawPassword: string, hashedPassword: string): Promise<boolean> {
+    return await bcrypt.compare(rawPassword, hashedPassword);
+  }
+
+  private static async generateSalt(): Promise<string> {
+    const saltRound = 8;
+    return await bcrypt.genSalt(saltRound);
+  }
+}
