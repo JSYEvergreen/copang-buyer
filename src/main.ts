d252d76c058e9e191c9e6b0b3e9da1b1cde2d3d4
FonDitbul <niki7084@naver.com>
2023-02-22 00:42:03 +0900

    [COU-0] exception nest setting

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './infrastructure/app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { HttpExceptionFilter } from './api/common/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalFilters(new HttpExceptionFilter());
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   await app.listen(3000);
 }
 bootstrap();
