c6da38fb9b403660053e8242955a4941fe29251f
FonDitbul <niki7084@naver.com>
2023-02-26 17:02:29 +0900

    [COU-2] login token 모듈 개발

---
+++ src/application/service/auth/token/login.jwt.token.spec.ts
@@ -0,0 +1,60 @@
+import { LoginJwtToken } from './login.jwt.token';
+import { UserInfo } from '../../../../domain/service/auth/token/login.token';
+import { CoPangException, EXCEPTION_STATUS } from '../../../../domain/common/exception';
+
+describe('Login Token 테스트', () => {
+  const sut = new LoginJwtToken();
+  const givenUser: UserInfo = {
+    id: 1,
+    userId: 'copang1234',
+  };
+
+  describe('getOne access token 과 refresh token 받아오기 테스트', () => {
+    test('주어진 정보로 인한 access token refresh token 발급 완료 ', () => {
+      const result = sut.getOne(givenUser);
+
+      expect(result).toHaveProperty('accessToken');
+      expect(result).toHaveProperty('refreshToken');
+    });
+  });
+
+  describe('access token verify 테스트', () => {
+    // before token 받아오기
+    const userOneLoginToken = sut.getOne(givenUser);
+    const accessToken = userOneLoginToken.accessToken.value;
+
+    test('올바른 access token 을 통한 user 정보 받아오기 성공', () => {
+      const result = sut.verifyByAccess(accessToken);
+
+      expect(result).toHaveProperty('id');
+      expect(result).toHaveProperty('userId');
+      expect(result.id).toEqual(givenUser.id);
+      expect(result.userId).toEqual(givenUser.userId);
+    });
+
+    test('잘못된 access token 을 통한 user 정보 받아오기 실패', () => {
+      const failedAccessToken = '1236512512';
+
+      expect(() => sut.verifyByAccess(failedAccessToken)).toThrow(new CoPangException(EXCEPTION_STATUS.LOGIN_TOKEN_ERROR));
+    });
+  });
+
+  describe('refresh token verify 테스트', () => {
+    // before token 받아오기
+    const userOneLoginToken = sut.getOne(givenUser);
+    const refreshToken = userOneLoginToken.refreshToken.value;
+
+    test('올바른 refresh token 을 통한 user 정보 받아오기 성공', () => {
+      const result = sut.verifyByRefresh(refreshToken);
+
+      expect(result).toHaveProperty('accessToken');
+      expect(result).toHaveProperty('refreshToken');
+    });
+
+    test('잘못된 refresh token 을 통한 user 정보 받아오기 실패', () => {
+      const failedAccessToken = '123651251222';
+
+      expect(() => sut.verifyByRefresh(failedAccessToken)).toThrow(new CoPangException(EXCEPTION_STATUS.LOGIN_TOKEN_ERROR));
+    });
+  });
+});
