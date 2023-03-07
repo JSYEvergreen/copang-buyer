ec585c48ddf08759c144fe312c3dd1c931e490dd
FonDitbul <niki7084@naver.com>
2023-03-07 23:36:23 +0900

    [COB-6] phoneNumber 관련 리팩토링

--- src/domain/service/buyer/port/buyer.out.ts
+++ src/domain/service/buyer/port/buyer.out.ts
@@ -1,8 +1,21 @@
-export interface BuyerSignUpOut {
-  userId: string;
-  password: string;
-  name: string;
-  nickName: string;
-  email: string;
+export class BuyerSignUpOut {
+  readonly userId: string;
+  readonly password: string;
+  readonly name: string;
+  readonly nickName: string;
+  readonly email: string;
   phoneNumber: string;
+
+  constructor(userId: string, password: string, name: string, nickName: string, email: string, phoneNumber: string) {
+    this.userId = userId;
+    this.password = password;
+    this.name = name;
+    this.nickName = nickName;
+    this.email = email;
+    this.phoneNumber = phoneNumber;
+  }
+
+  regPhoneNumber() {
+    this.phoneNumber = this.phoneNumber.trim().replace(/-/g, '').replace(/(\s*)/g, '');
+  }
 }
