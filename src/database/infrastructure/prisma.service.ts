17f6622bbcad915823c9af8e77df9720262bce6e
FonDitbul <niki7084@naver.com>
2023-07-14 18:18:01 +0900

    add: prisma DB log 기능 추가

--- src/database/infrastructure/prisma.service.ts
+++ src/database/infrastructure/prisma.service.ts
@@ -1,9 +1,35 @@
-import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
+import { INestApplication, Injectable, Logger, OnModuleInit } from '@nestjs/common';
+import { Prisma, PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit {
+export class PrismaService extends PrismaClient<Prisma.PrismaClientOptions, 'query'> implements OnModuleInit {
+  private readonly logger = new Logger(PrismaService.name);
+  constructor() {
+    super({
+      log: [
+        {
+          emit: 'event',
+          level: 'query',
+        },
+        {
+          emit: 'event',
+          level: 'error',
+        },
+        {
+          emit: 'stdout',
+          level: 'info',
+        },
+        {
+          emit: 'stdout',
+          level: 'warn',
+        },
+      ],
+    });
+  }
   async onModuleInit() {
+    this.$on('query', (e) => {
+      this.logger.verbose(e.query);
+    });
     await this.$connect();
   }
 
